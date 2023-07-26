df862479f8b1019a322e204764b11b6c551ea827
Marcel de Zanna <marceldezanna@yahoo.de>
2023-07-26 21:04:34 +0200

    first components

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from './components/button/button.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ButtonModule
+  ],
+  exports: [
+    ButtonModule
+  ]
+})
+export class SharedModule { }
