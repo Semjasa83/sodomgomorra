20789d2f88af33c5d29c533c606a27e18bbb4855
Marcel de Zanna <marceldezanna@yahoo.de>
2023-07-27 22:21:05 +0200

    new buttons

---
+++ src/app/modules/shared/components/interfacebutton/interfacebutton.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InterfacebuttonComponent } from './interfacebutton/interfacebutton.component';
+
+
+
+@NgModule({
+  declarations: [
+    InterfacebuttonComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    InterfacebuttonComponent
+  ]
+})
+export class InterfacebuttonModule { }
---
+++ src/app/modules/shared/components/interfacebutton/interfacebutton/interfacebutton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sg-ibutton',
+  templateUrl: './interfacebutton.component.html',
+  styleUrls: ['./interfacebutton.component.scss']
+})
+export class InterfacebuttonComponent {
+
+}
