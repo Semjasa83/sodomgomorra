dcae8424e433b9c167369f374d47d1fd24a10ca8
Marcel de Zanna <marceldezanna@yahoo.de>
2023-08-14 13:45:26 +0200

    generated dropdown - no function atm

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from './components/button/button.module';
 import { MenubarModule } from './components/menubar/menubar.module';
+import {DropdownModule} from "./components/dropdown/dropdown.module";
+import {DropdownComponent} from "./components/dropdown/dropdown/dropdown.component";
+
 
 @NgModule({
   declarations: [
+
   ],
   imports: [
     CommonModule,
     ButtonModule,
-    MenubarModule
+    MenubarModule,
+    DropdownModule
   ],
   exports: [
     ButtonModule,
-    MenubarModule
+    MenubarModule,
+    DropdownComponent
   ]
 })
 export class SharedModule { }
