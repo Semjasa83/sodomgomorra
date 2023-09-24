a11f59f71bb5f08c59f1b912816bd51b644729af
Marcel de Zanna <marceldezanna@yahoo.de>
2023-09-24 14:21:47 +0200

    generated dropdown - no function atm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './modules/shared/shared.module';
-import { LoginComponent } from './modules/login/login.component';
-import { GameinterfaceComponent } from './modules/gameinterface/gameinterface.component';
-
+import { GameinterfaceComponent } from "./interface/gameinterface/gameinterface.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    GameinterfaceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
+    GameinterfaceComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
